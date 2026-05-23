6cb12ecfccf828ae994ac03d21590d052b2d23c0
João Barreto <anothermailb@gmail.com>
2026-05-23 15:22:49 -0300

    Implement Cargo in the project and start developing "guessing_game.rs".

---
+++ learning/guessing_game/target/debug/deps/guessing_game-2b95446ec93daabb.d
@@ -0,0 +1,5 @@
+/home/joaob/projects/rust/learning/guessing_game/target/debug/deps/guessing_game-2b95446ec93daabb.d: src/main.rs
+
+/home/joaob/projects/rust/learning/guessing_game/target/debug/deps/guessing_game-2b95446ec93daabb: src/main.rs
+
+src/main.rs:
